7592cf3eb89f89e9e8ef0c13adabb33b5f0ca8f1
Alexis Rondeau <alexis.rondeau@gmail.com>
2023-03-11 09:55:48 +0100

    Update

--- publish.sh
+++ publish.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+echo "Committing latest to Github:"
+echo ""
 ./ship.sh
+
+echo "Generating DMG:"
+echo ""
 ./dmg.sh
+
+echo "Generating Appcast:"
+echo ""
 ./generate_appcast.sh
+
+echo "Cleaning up:"
+echo ""
+./cleanup.sh 
